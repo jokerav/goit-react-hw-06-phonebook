da0910370085f3399f19d62d37eb998d130419c4
jokerav <50701756+jokerav@users.noreply.github.com>
2022-07-04 22:11:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,6 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 export const App = () => {
-  // const isNameInPhonebook = name => {
-  //   const nameInLowerCase = name.toLowerCase();
-  //   for (const contact of contacts) {
-  //     if (contact.name.toLowerCase() === nameInLowerCase) {
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // };
-  // const addContact = contact => {
-  //   const { name } = contact;
-  //   if (!isNameInPhonebook(name)) {
-  //     contact.id = nanoid(5);
-  //     setContacts(prevContacts => [...prevContacts, contact]);
-  //   } else {
-  //     alert(`${name} is already in contacts`);
-  //   }
-  // };
-
   return (
     <div
       style={{
