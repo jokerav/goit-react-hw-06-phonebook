8099dbe4d7f00107dd89000f2f6b4791299690bb
jokerav <50701756+jokerav@users.noreply.github.com>
2022-07-04 14:21:21 +0300

    createReducer

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,16 @@
 import { nanoid } from 'nanoid';
+import { createAction } from '@reduxjs/toolkit';
 
-const addContact = contact => ({
-  type: 'CONTACT/ADD',
+const addContact = createAction('CONTACT/ADD', contact => ({
   payload: {
     id: nanoid(5),
     name: contact.name,
     number: contact.number,
   },
-});
-const deleteContact = id => ({
-  type: 'CONTACT/DELETE',
-  payload: id,
-});
-const changeFilter = text => ({ type: 'FILTER/CHANGE', payload: text });
+}));
+
+const deleteContact = createAction('CONTACT/DELETE');
+
+const changeFilter = createAction('FILTER/CHANGE');
+
 export { addContact, deleteContact, changeFilter };
--- src/redux/contacts-reducer.js
+++ src/redux/contacts-reducer.js
@@ -1,21 +1,31 @@
 import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
-const items = (state = [], { type, payload }) => {
-  switch (type) {
-    case 'CONTACT/ADD':
-      return [...state, payload];
-    case 'CONTACT/DELETE':
-      return state.filter(({ id }) => id !== payload);
-    default:
-      return state;
-  }
-};
-const filter = (state = '', { type, payload }) => {
-  switch (type) {
-    case 'FILTER/CHANGE':
-      return payload;
-    default:
-      return state;
-  }
-};
+const items = createReducer([], {
+  'CONTACT/ADD': (state, { payload }) => [...state, payload],
+  'CONTACT/DELETE': (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
+const filter = createReducer('', {
+  'FILTER/CHANGE': (state, { payload }) => payload,
+});
+
+// const items = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case 'CONTACT/ADD':
+//       return [...state, payload];
+//     case 'CONTACT/DELETE':
+//       return state.filter(({ id }) => id !== payload);
+//     default:
+//       return state;
+//   }
+// };
+// const filter = (state = '', { type, payload }) => {
+//   switch (type) {
+//     case 'FILTER/CHANGE':
+//       return payload;
+//     default:
+//       return state;
+//   }
+// };
 export default combineReducers({ items, filter });
